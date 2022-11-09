dffe612520eeb1dc72ec87b114d83f3748cc88ee
Héctor Peñalva Garcia <hectorpenalvagarcia@icloud.com>
2022-11-10 00:23:01 +0100

    Code refactor

--- src/components/Gifs.js
+++ src/components/Gifs.js
@@ -3,7 +3,12 @@ import Figure from "react-bootstrap/Figure";
 export default function Gif({ gif }) {
   return (
     <Figure key={gif.id}>
-      <Figure.Image height={100} alt={gif.title} src={gif.url} />
+      <Figure.Image
+        className="shadow m-1"
+        height={100}
+        alt={gif.title}
+        src={gif.url}
+      />
       <Figure.Caption>{gif.title}</Figure.Caption>
     </Figure>
   );
