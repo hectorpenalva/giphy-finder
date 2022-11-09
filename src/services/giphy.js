e84b6e53ffee60734b8c495823db697389785d31
Héctor Peñalva <hectorpenalvagarcia@icloud.com>
2022-11-09 21:57:39 +0100

    Service giphy.js to fetch the gifs by keyword

--- src/services/giphy.js
+++ src/services/giphy.js
@@ -1,10 +1,16 @@
 const API_KEY = "bVzSbFfc3MbDubLmRTc7LNq64IWEey7S";
 
-export default function gifFetch(query, limit) {
-  fetch(
+export default async function gifFetch(query, limit) {
+  const response = await fetch(
     `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=${limit}&offset=0&rating=g&lang=es`
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
+  );
+
+  const data = await response.json();
+  const gifsArray = data.data.map((gif) => {
+    const { images, title, id } = gif;
+    const { url } = images.fixed_height_downsampled;
+    return { url, title, id };
+  });
+
+  return gifsArray;
 }
