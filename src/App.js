c96ffde200da843593d87bc3a5a4275d79a9c6fa
Héctor Peñalva Garcia <hectorpenalvagarcia@icloud.com>
2022-11-10 00:24:10 +0100

    App.js refactor

--- src/App.js
+++ src/App.js
@@ -1,49 +1,27 @@
 import { useState } from "react";
 import "./App.css";
-import ListOfGif from "./components/ListOfGifs";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Menu from "./components/Menu";
+import SubMenu from "./components/SubMenu";
+import ListOfGif from "./components/ListOfGifs";
 
 export default function App() {
-  const [keyword, setKeyword] = useState("panda");
   const [showGifs, setShowGifs] = useState(true);
-
-  const handleChange = (event) => {
-    setKeyword(event.target.value);
-  };
+  const [keyword, setKeyword] = useState("panda");
 
   return (
     <div className="App">
-      <Form
-        className="App d-flex align-items-center justify-content-center"
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Que Gifs quieres buscar?</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter gif"
-            onChange={handleChange}
-          />
-        </Form.Group>
-      </Form>
-      <Button variant="outline-primary" onClick={() => setShowGifs(!showGifs)}>
-        {showGifs ? "OCULTAR" : "MOSTRAR"} GIFS
-      </Button>{" "}
-      <Button variant="outline-primary" onClick={() => setKeyword("españa")}>
-        ESPAÑA
-      </Button>{" "}
-      <Button
-        variant="outline-primary"
-        onClick={() => setKeyword("breakingbad")}
-      >
-        BREAKING BAD
-      </Button>{" "}
-      {showGifs ? <ListOfGif keyword={keyword}></ListOfGif> : "NO HAY GIFS"}
+      <Menu setKeyword={setKeyword}></Menu>
+      <SubMenu
+        setKeyword={setKeyword}
+        showGifs={showGifs}
+        setShowGifs={setShowGifs}
+      ></SubMenu>
+      <Container>
+        {showGifs ? <ListOfGif keyword={keyword}></ListOfGif> : "GIFS OCULTOS"}
+      </Container>
     </div>
   );
 }
